e0128d267e1b54903362ad00ba15bcbae9a25f20
Christoph Pageler <christoph.pageler@me.com>
2024-10-05 20:46:40 +0200

    Cleanup

--- Sources/HomeControlKit/WebSockets/WebSocketMessage.swift
+++ Sources/HomeControlKit/WebSockets/WebSocketMessage.swift
@@ -13,26 +13,3 @@ public protocol WebSocketMessage: Codable {
     var identifier: String { get }
     var content: Content { get set }
 }
-
-//public extension URLSessionWebSocketTask.Message {
-//    var webSocketMessage: (any WebSocketMessage)? {
-//        switch self {
-//        case .string(_):
-//            return nil
-//        case let .data(data):
-//            let messageTypes: [any WebSocketMessage.Type] = [
-//                WebSocketDidRegisterMessage.self,
-//                WebSocketDidUpdateLatestInverterReadingMessage.self,
-//                WebSocketPingMessage.self
-//            ]
-//            for messageType in messageTypes {
-//                if let decoded = try? JSONDecoder().decode(messageType, from: data) {
-//                    return decoded
-//                }
-//            }
-//            return nil
-//        default:
-//            return nil
-//        }
-//    }
-//}
